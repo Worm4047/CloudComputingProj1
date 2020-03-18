cba6ea1448ba33a270747ccd4046fcc75745c6f7
Abhishek Chaudhary <abhidagar@rocketmail.com>
2020-03-17 18:24:17 -0700

    Surv

---
+++ survellance.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+'''
+SETUP:
+
+    -   -->     GND     -->     PIN6
+    +   -->     5V      -->     PIN4
+    S   -->     GPIO18  -->     PIN12
+
+'''
+
+import RPi.GPIO as GPIO
+import subprocess
+import time
+import sys
+import os
+
+sensor = 12
+PATH_CLOUD = "/home/pi/CloudComputingProj1"
+PATH_FACEDETECT = "/home/pi/facedetect"
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN)
+
+on = 0
+off = 0
+flag = 0
+while flag == 0:
+    i=GPIO.input(sensor)
+    if i == 0:
+        if flag == 1:
+            off = time.time()
+            diff = off - on
+            print 'time: ' + str(diff%60) + ' sec'
+            print ''
+            flag = 0
+        print "No intruders"
+        time.sleep(1)
+    elif i == 1:
+        if flag == 0:
+            print "Intruder detected"
+            on = time.time()
+            flag = 1
+            process = subprocess.Popen('python take_snapshot.py v.h264', shell=True)
+            process.wait()
+            os.chdir(PATH_CLOUD)
+            process = subprocess.Popen('python uploadFile.py ' + PATH_FACEDETECT+'/v.h264', shell=True)
+        time.sleep(0.1)
