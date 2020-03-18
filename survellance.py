99d85a649dc0b1a66873ec17b1664960559acb57
Abhishek Chaudhary <abhidagar@rocketmail.com>
2020-03-17 18:41:22 -0700

    Surv

--- survellance.py
+++ survellance.py
@@ -25,25 +25,24 @@ GPIO.setup(sensor, GPIO.IN)
 on = 0
 off = 0
 flag = 0
-while flag == 0:
+while True:
+    time.sleep(5)
     i=GPIO.input(sensor)
     if i == 0:
-        if flag == 1:
-            off = time.time()
-            diff = off - on
-            print 'time: ' + str(diff%60) + ' sec'
-            print ''
-            flag = 0
+        off = time.time()
+        diff = off - on
+        print 'time: ' + str(diff%60) + ' sec'
+        print ''
         print "No intruders"
         time.sleep(1)
     elif i == 1:
-        if flag == 0:
-            print "Intruder detected"
-            on = time.time()
-            flag = 1
-            process = subprocess.Popen('python take_snapshot.py v.h264', shell=True)
-            process.wait()
-            os.chdir(PATH_CLOUD)
-            process = subprocess.Popen('python uploadFile.py ' + PATH_FACEDETECT+'/v.h264', shell=True)
-            os.chdir(PATH_FACEDETECT)
+        print "Intruder detected"
+        on = time.time()
+        flag = 1
+        process = subprocess.Popen('python take_snapshot.py v.h264', shell=True)
+        process.wait()
+        os.chdir(PATH_CLOUD)
+        process = subprocess.Popen('python uploadFile.py ' + PATH_FACEDETECT+'/v.h264', shell=True)
+        process.wait()
+        os.chdir(PATH_FACEDETECT)
         time.sleep(0.1)
